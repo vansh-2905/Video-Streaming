6a65db5b1320bcba4ee796852456c75d7e0476d3
vansh <vanshkapoor29503@gmail.com>
2024-09-05 23:37:29 -0700

    create video platform

---
+++ frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import VideoPlayer from './VideoPlayer'
+import { useRef } from 'react'
+
+function App() {
+  const playerRef = useRef(null)
+  const videoLink = "http://localhost:8000/uploads/testUploads/a8f59acf-0ad3-471f-8020-eb03be8f8ed2/index.m3u8"
+
+  const videoPlayerOptions = {
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: videoLink,
+        type: "application/x-mpegURL"
+      }
+    ]
+  }
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+  return (
+    <>
+      <div>
+        <h1>Video player</h1>
+      </div>
+      <VideoPlayer
+      options={videoPlayerOptions}
+      onReady={handlePlayerReady}
+      />
+    </>
+  )
+}
+
+export default App
